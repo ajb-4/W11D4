26cde84746e226e5f6f3afde20df5a3cc9fd9283
ajb-4 <alexbrown4711@gmail.com>
2023-09-07 17:25:34 -0400

    last commit

--- practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/config/routes.rb
+++ practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api, default: {format: :json} do 
+    resources :pokemon, only: [:show, :index, :create, :update]
+  end
 end
