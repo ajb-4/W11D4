a2108481d4ace2e0725977cc4f6bf80fa44b33f0
ajb-4 <alexbrown4711@gmail.com>
2023-09-07 14:33:59 -0400

    done with models

--- practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/models/move.rb
+++ practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/models/move.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: moves
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Move < ApplicationRecord
+    
+    validates :name, uniqueness: {message: "'%{value}' is already in use"}
+
 end
